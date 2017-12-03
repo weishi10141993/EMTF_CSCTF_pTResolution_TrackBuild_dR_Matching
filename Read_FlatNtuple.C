77c3045e555846a691fae87a70bb9c17849623cb
weishi10141993 <weishi@rice.edu>
2017-12-03 14:30:33 +0100

    Update Read_FlatNtuple.C

--- Read_FlatNtuple.C
+++ Read_FlatNtuple.C
@@ -66,9 +66,9 @@ void Read_FlatNtuple() {
     in_chain->Add( in_file_names.at(i) );
   }
   TString Topology4Title="";
-  Topology4Title = Topology4Title + "dTh cuts 4-station tracks: pT>="+ Form("%d", PT_CUT) +"GeV, eta ["+Form("%.1f", ETA_LOW)+","+ Form("%.1f", ETA_UP)+"]";
+  Topology4Title = Topology4Title + "dTh cuts 4-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, eta ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
   TString Topology3Title="";
-  Topology3Title = Topology3Title + "dTh cuts 3-station tracks: pT>="+ Form("%d", PT_CUT) +"GeV, eta ["+Form("%.1f", ETA_LOW)+","+ Form("%.1f", ETA_UP)+"]";
+  Topology3Title = Topology3Title + "dTh cuts 3-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, eta ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
   TH2F *CutTopology4 = new TH2F("CutTopology4", Topology4Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
   TH2F *CutTopology3 = new TH2F("CutTopology3", Topology3Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
   
