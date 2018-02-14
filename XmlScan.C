bc6f96a3036bc21cd6a72338de5fa138ff168547
Wei Shi <weishi@rice.edu>
2018-02-14 20:48:51 +0100

    Update XmlScan.C

--- XmlScan.C
+++ XmlScan.C
@@ -50,6 +50,9 @@ void XmlScan()
                 while(!f.eof()) {
                         getline(f,line);
                         cout << "data line " << getlinecount << ": " << line << endl;
+                        char *ret;
+                        ret = strstr(line, "boostWeight="");
+                        printf("The substring is: %s\n", ret);
                         getlinecount++;
                 }
        
