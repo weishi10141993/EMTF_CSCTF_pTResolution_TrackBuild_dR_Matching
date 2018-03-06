944baa65a7eab569cfae464c84a08bd06eb12ffb
Wei Shi <weishi@rice.edu>
2018-03-06 14:23:33 +0100

    Update Read_FlatNtuple.h

--- Read_FlatNtuple.h
+++ Read_FlatNtuple.h
@@ -39,7 +39,10 @@ std::vector<TString> vInt = {{
     "unp_trk_pt_int", "unp_trk_eta_int", "unp_trk_theta_int", "unp_trk_phi_int", "unp_trk_BX","unp_trk_endcap",
     "unp_trk_sector", "unp_trk_sector_index", "unp_trk_mode", "unp_trk_mode_CSC", "unp_trk_mode_RPC", "unp_trk_mode_neighbor",
     "unp_trk_charge", "unp_trk_nHits", "unp_trk_nRPC", "unp_trk_nNeighbor", "unp_trk_found_hits",
-    "unp_trk_dBX", "unp_trk_dPhi_int", "unp_trk_dTheta_int"
+    "unp_trk_dBX", "unp_trk_dPhi_int", "unp_trk_dTheta_int",
+	
+    "reco_ID_PF", "reco_ID_global", "reco_ID_loose", "reco_ID_medium", "reco_ID_nStations", "reco_ID_soft", "reco_ID_stand", 
+    "reco_ID_station", "reco_ID_tight", "reco_ID_tracker", "reco_charge", "reco_dR_match_iTrk", "reco_dR_match_nTrk", "reco_dR_match_unique"
   }};
 std::vector<TString> vvInt = {{
     "trk_iHit",
