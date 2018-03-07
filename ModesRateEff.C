9ee0293192ea2a44f08cfdd08f8d21b68398e592
Wei Shi <weishi@rice.edu>
2018-03-07 07:53:29 +0100

    Update and rename ModesRateEfficiency.C to ModesRateEff.C

---
+++ ModesRateEff.C
@@ -0,0 +1,158 @@
+#include "TFile.h"
+#include "TSystem.h"
+#include "TChain.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TH2.h"
+
+#include "Read_FlatNtuple.h" // List of input branches and functions to return values
+
+//USER modify here ONLY//
+//================================================================
+const bool verbose  = true; // Print information
+const float ETA_UP = 2.4;
+const float ETA_LOW = 1.25;
+const int MAX_EVT   = 1000000;   // Max number of events to process
+const int PRT_EVT   =  1;   // Print every N events
+//================================================================
+
+void ModesRateEff() {
+   
+  // Initialize empty file to access each file in the list
+  TFile *file_tmp(0);
+
+  // List of input files
+  std::vector<TString> SM_in_file_names;//SingleMu
+  std::vector<TString> ZB_in_file_names;//Zerobias
+  TString store = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/";
+  TString in_dir = "ntuples/HADD/";
+  TString SM_file_name;
+  TString ZB_file_name;
+
+  TString SingleMu_files[3] = { 
+	  		    "NTuple_SingleMuon_FlatNtuple_Run_306092_2018_03_02_SingleMu.root",
+   			    "NTuple_SingleMuon_FlatNtuple_Run_306135_2018_03_02_SingleMu.root",
+   			    "NTuple_SingleMuon_FlatNtuple_Run_306154_2018_03_02_SingleMu.root"
+  };   
+  TString ZeroBias_files[4] = { 
+	  		    "NTuple_ZeroBias1_FlatNtuple_Run_306091_2018_03_02_ZB1.root",
+		            "NTuple_ZeroBias2_FlatNtuple_Run_306091_2018_03_02_ZB2.root",
+		            "NTuple_ZeroBias3_FlatNtuple_Run_306091_2018_03_02_ZB3.root", 
+		 	    "NTuple_ZeroBias4_FlatNtuple_Run_306091_2018_03_02_ZB4.root"
+  };
+  for (int i = 0; i < 3; i++) {
+	  SM_file_name.Form( "%s/%s/%s", store.Data(), in_dir.Data(), SingleMu_files[i].Data() );
+  	  std::cout << "Adding file " << SM_file_name.Data() << std::endl;
+          SM_in_file_names.push_back(SM_file_name.Data());
+  }
+  for (int i = 0; i < 4; i++) {
+	  ZB_file_name.Form( "%s/%s/%s", store.Data(), in_dir.Data(), ZeroBias_files[i].Data() );
+  	  std::cout << "Adding file " << ZB_file_name.Data() << std::endl;
+          ZB_in_file_names.push_back(ZB_file_name.Data());
+  }
+  
+  // Open all input files
+  for (int i = 0; i < SM_in_file_names.size(); i++) {
+    if ( !gSystem->AccessPathName(SM_in_file_names.at(i)) )
+      file_tmp = TFile::Open( SM_in_file_names.at(i) ); // Check if file exists
+    if (!file_tmp) {
+      std::cout << "ERROR: could not open data file " << SM_in_file_names.at(i) << std::endl;
+      return;
+    }
+  }
+
+  for (int i = 0; i < ZB_in_file_names.size(); i++) {
+    if ( !gSystem->AccessPathName(ZB_in_file_names.at(i)) )
+      file_tmp = TFile::Open( ZB_in_file_names.at(i) ); // Check if file exists
+    if (!file_tmp) {
+      std::cout << "ERROR: could not open data file " << ZB_in_file_names.at(i) << std::endl;
+      return;
+    }
+  }
+	
+  // Add tree from the input files to the TChain
+  TChain *SM_in_chain= new TChain("FlatNtupleData/tree");
+  for (int i = 0; i < SM_in_file_names.size(); i++) {
+    SM_in_chain->Add( SM_in_file_names.at(i) );
+  }
+	
+  TChain *ZB_in_chain= new TChain("FlatNtupleData/tree");
+  for (int i = 0; i < ZB_in_file_names.size(); i++) {
+    ZB_in_chain->Add( ZB_in_file_names.at(i) );
+  }
+	
+  TString Topology4Title="";
+  Topology4Title = Topology4Title + "dTh cuts 4-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, abs(eta) ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
+  TString Topology3Title="";
+  Topology3Title = Topology3Title + "dTh cuts 3-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, abs(eta) ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
+  TH2F *CutTopology4 = new TH2F("CutTopology4", Topology4Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
+  TH2F *CutTopology3 = new TH2F("CutTopology3", Topology3Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
+  
+  InitializeMaps();
+  SetBranchAddresses(SM_in_chain);
+  SetBranchAddresses(ZB_in_chain);
+	
+  std::cout << "\n******* About to loop over the SingleMu events *******" << std::endl;
+  int nEvents = SM_in_chain->GetEntries();
+  for (int iEvt = 0; iEvt < nEvents; iEvt++) {
+    if (iEvt > MAX_EVT) break;
+    if ( (iEvt % PRT_EVT) == 0 ) {
+      std::cout << "\n*************************************" << std::endl;
+      std::cout << "Looking at event " << iEvt << " out of " << nEvents << std::endl;
+      std::cout << "*************************************" << std::endl;
+    }
+
+    SM_in_chain->GetEntry(iEvt);
+    
+    // From Read_FlatNtuple.h, use 'I("branch_name")' to get an integer branch value, 'F("branch_name") to get a float
+    // Print info for unpacked EMTF tracks
+    if (verbose) std::cout << "\n" << I("nUnpTracks") << " unpacked tracks in the event" << std::endl;
+    for (int itrack = 0; itrack < I("nUnpTracks"); itrack++) {
+	     
+    }//end loop over unpacked tracks
+    
+  } // End loop events
+  std::cout << "\n******* Finished looping over the SingleMu events *******" << std::endl;
+  
+  std::cout << "\n******* About to loop over the Zerobias events *******" << std::endl;
+  int nEvents = ZB_in_chain->GetEntries();
+  for (int iEvt = 0; iEvt < nEvents; iEvt++) {
+    if (iEvt > MAX_EVT) break;
+    if ( (iEvt % PRT_EVT) == 0 ) {
+      std::cout << "\n*************************************" << std::endl;
+      std::cout << "Looking at event " << iEvt << " out of " << nEvents << std::endl;
+      std::cout << "*************************************" << std::endl;
+    }
+
+    ZB_in_chain->GetEntry(iEvt);
+    
+    // From Read_FlatNtuple.h, use 'I("branch_name")' to get an integer branch value, 'F("branch_name") to get a float
+    // Print info for unpacked EMTF tracks
+    if (verbose) std::cout << "\n" << I("nUnpTracks") << " unpacked tracks in the event" << std::endl;
+    for (int itrack = 0; itrack < I("nUnpTracks"); itrack++) {
+	     
+    }//end loop over unpacked tracks
+    
+  } // End loop events
+  std::cout << "\n******* Finished looping over the Zerobias events *******" << std::endl;
+
+  delete SM_in_chain;
+  delete ZB_in_chain;
+  std::cout << "\nDone with macro. Exiting.\n" << std::endl;
+	
+  //write to output file
+  TString outFile = "/afs/cern.ch/work/w/wshi/public/EMTFPileUp/ModesRateEff";
+  outFile = outFile + "_eta_" + Form("%.1f", ETA_LOW) + "_" + Form("%.1f", ETA_UP) + ".root";
+  TFile myPlot(outFile,"RECREATE");
+        
+  CutTopology4->GetXaxis()->SetTitle("dTheta(1-X)");
+  CutTopology4->GetYaxis()->SetTitle("dTheta(X-Y)");
+  CutTopology4->Write();
+        
+  CutTopology3->GetXaxis()->SetTitle("dTheta(1-X)");
+  CutTopology3->GetYaxis()->SetTitle("dTheta(X-Y)");
+  CutTopology3->Write();
+        
+  myPlot.Close();
+  
+} // End function: void 
