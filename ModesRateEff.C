bc93af5ee553439ed5c87d699a3d03978e1151e3
Wei Shi <weishi@rice.edu>
2018-03-14 11:48:34 +0100

    Update ModesRateEff.C

--- ModesRateEff.C
+++ ModesRateEff.C
@@ -494,7 +494,7 @@ void ModesRateEff() {
     // Print info for unpacked EMTF tracks
     if (verbose) std::cout << "\n" << I("nTracks") << " tracks in the event" << std::endl;
     for (int itrack = 0; itrack < I("nTracks"); itrack++) {
-	    if( I("trk_BX", itrack) == 0 && fabs( F("trk_eta", itrack) ) >= ETA_LOW ){
+	    if( I("trk_BX", itrack) == 0 && fabs( F("trk_eta", itrack) ) >= ETA_LOW && I("trk_mode_neighbor", itrack ) < I("trk_mode", itrack ) ){
 		    for(int i=0;i<30;i++){
 			    
 			    if( F("trk_pt", itrack)> TrigPT[i]){
