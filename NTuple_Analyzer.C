7883a93648baa5b87d3d72b6ab4dd317a6a8810a
Wei Shi <weishi@rice.edu>
2018-04-24 14:30:46 +0200

    Create NTuple_Analyzer.C

---
+++ NTuple_Analyzer.C
@@ -0,0 +1,260 @@
+////////////////////////////////////////////////////////////
+///             Adapted from                             ///
+///      Simple macro to read EMTF Flat NTuples          ///
+///      Andrew Brinkerhoff 29.09.17                     ///
+///                                                      ///
+///   TChain can be used to read multiple files.         ///
+///   Format: interface/FlatNtupleBranches.h             ///
+////////////////////////////////////////////////////////////
+
+#include "TFile.h"
+#include "TSystem.h"
+#include "TChain.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TH2.h"
+
+#include "Read_FlatNtuple.h" // List of input branches and functions to return values
+
+//USER modify here ONLY//
+//================================================================
+const int MAX_FILES = 1;   // Max number of files to process
+const int MAX_EVT   = 1000000;   // Max number of events to process
+const int PRT_EVT   =  1;   // Print every N events
+const bool verbose  = true; // Print information about the event and RECO and L1T muon
+const int PT_CUT = 22;
+const float ETA_UP = 2.4;
+const float ETA_LOW = 1.6;
+const int CUT1_UP = 16;//dTheta(1-X)
+const int CUT1_LOW = 0;
+const int CUT2_UP = 8;//dTheta(X-Y)
+const int CUT2_LOW = 0;
+//================================================================
+
+///////////////////////////////////////////
+///  Main function to read the NTuples  ///
+///////////////////////////////////////////
+
+void NTuple_Analyzer() {
+   
+  // Initialize empty file to access each file in the list
+  TFile *file_tmp(0);
+
+  // List of input files
+  std::vector<TString> in_file_names;
+  TString store = "root://eoscms.cern.ch//store/user/abrinke1/EMTF/Emulator/";
+  TString in_dir = "ntuples/HADD/";
+  TString file_name;
+
+  file_name.Form("%s/%s/NTuple_ZeroBias8b4e_FlatNtuple_Skim_Run_302674_2017_09_30.root", store.Data(), in_dir.Data());
+  std::cout << "Adding file " << file_name.Data() << std::endl;
+  in_file_names.push_back(file_name.Data());
+
+  // Open all input files
+  for (int i = 0; i < in_file_names.size(); i++) {
+    if ( !gSystem->AccessPathName(in_file_names.at(i)) )
+      file_tmp = TFile::Open( in_file_names.at(i) ); // Check if file exists
+    if (!file_tmp) {
+      std::cout << "ERROR: could not open data file " << in_file_names.at(i) << std::endl;
+      return;
+    }
+  }
+
+  // Add tree from the input files to the TChain
+  TChain *in_chain= new TChain("ntuple/tree");
+  for (int i = 0; i < in_file_names.size(); i++) {
+    in_chain->Add( in_file_names.at(i) );
+  }
+  TString Topology4Title="";
+  Topology4Title = Topology4Title + "dTh cuts 4-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, abs(eta) ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
+  TString Topology3Title="";
+  Topology3Title = Topology3Title + "dTh cuts 3-station tracks: pT >= "+ Form("%d", PT_CUT) +"GeV, abs(eta) ["+Form("%.1f", ETA_LOW)+", "+ Form("%.1f", ETA_UP)+"]";
+  TH2F *CutTopology4 = new TH2F("CutTopology4", Topology4Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
+  TH2F *CutTopology3 = new TH2F("CutTopology3", Topology3Title, CUT1_UP-CUT1_LOW+2, CUT1_LOW-1, CUT1_UP+1, CUT2_UP-CUT2_LOW+2, CUT2_LOW-1, CUT2_UP+1);
+  
+  InitializeMaps();
+  SetBranchAddresses(in_chain);
+	
+  std::cout << "\n******* About to loop over the events *******" << std::endl;
+  int nEvents = in_chain->GetEntries();
+  for (int iEvt = 0; iEvt < nEvents; iEvt++) {
+    if (iEvt > MAX_EVT) break;
+    if ( (iEvt % PRT_EVT) == 0 ) {
+      std::cout << "\n*************************************" << std::endl;
+      std::cout << "Looking at event " << iEvt << " out of " << nEvents << std::endl;
+      std::cout << "*************************************" << std::endl;
+    }
+
+    in_chain->GetEntry(iEvt);
+    
+    // From Read_FlatNtuple.h, use 'I("branch_name")' to get an integer branch value, 'F("branch_name") to get a float
+    // Print info for unpacked EMTF tracks
+    if (verbose) std::cout << "\n" << I("nUnpTracks") << " unpacked tracks in the event" << std::endl;
+    for (int itrack = 0; itrack < I("nUnpTracks"); itrack++) {
+	    
+	    int th1=-99;
+	    int th2=-99;
+	    int th3=-99;
+	    int th4=-99;
+	    int dTh12=-99;
+	    int dTh13=-99;
+	    int dTh14=-99;
+	    int dTh23=-99;
+	    int dTh24=-99;
+	    int dTh34=-99;
+	    int dTh1X=-99;
+	    int dThXY=-99;
+
+	    if(fabs(I("unp_trk_BX", itrack)) <= 1 && I("unp_trk_nHits",itrack) >= 3 && F("unp_trk_pt", itrack) >= PT_CUT && I("unp_trk_nRPC",itrack) == 0 && I("unp_trk_found_hits",itrack) == 1 && fabs(F("unp_trk_eta",itrack)) >= ETA_LOW && fabs(F("unp_trk_eta",itrack)) <= ETA_UP){
+		    
+		    if (verbose) std::cout << " * Mode " << I("unp_trk_mode", itrack) << " nHits:"<<I("unp_trk_nHits",itrack)<< " track with BX = " << I("unp_trk_BX", itrack) << ", pT = " << F("unp_trk_pt", itrack) << ", eta = " << F("unp_trk_eta", itrack) << ", phi = " << F("unp_trk_phi", itrack) << ", max dPhi_int among hits = " << I("unp_trk_dPhi_int", itrack) << std::endl;
+		    
+	            for (int jhit = 0; jhit < I("unp_trk_nHits", itrack); jhit++) {
+			    
+			    int iHit = I("unp_trk_iHit", itrack, jhit);  // Access the index of each hit in the selected track
+			    if (verbose) std::cout << "  - LCT with BX = " << I("hit_BX", iHit) << ", endcap = " << I("hit_endcap", iHit) << ", station = " << I("hit_station", iHit) << ", ring = " << I("hit_ring", iHit) <<", theta = "<< I("hit_theta_int", iHit);<< std::endl; 
+				    
+			    if( I("hit_station", iHit) ==1 ){
+				    th1=I("hit_theta_int", iHit);
+		            }
+			    else if ( I("hit_station", iHit) == 2 ){
+			            th2=I("hit_theta_int", iHit);
+			    }
+			    else if ( I("hit_station", iHit) == 3 ){
+				    th3=I("hit_theta_int", iHit);
+			    }
+		            else if ( I("hit_station", iHit) == 4 ){
+			            th4=I("hit_theta_int", iHit);
+			    }
+		    }//end loop over hits in selected unpacked track
+		    //4-station
+		    if (I("unp_trk_mode", itrack) == 15){
+			    dTh12=fabs(th1-th2);
+			    dTh13=fabs(th1-th3);
+			    dTh14=fabs(th1-th4);
+			    dTh23=fabs(th2-th3);
+	                    dTh24=fabs(th2-th4);
+	                    dTh34=fabs(th3-th4);
+			    dTh1X=dTh12>dTh13?dTh12:dTh13;
+			    dTh1X=dTh1X>dTh14?dTh1X:dTh14;
+			    dThXY=dTh23>dTh24?dTh23:dTh24;
+			    dThXY=dThXY>dTh34?dThXY:dTh34;
+			    //no cut bin
+			    CutTopology4->Fill(CUT1_LOW-1,CUT2_LOW-1);
+			    //loop over cut on dTheta(1-X)
+                            for(Int_t i=CUT1_LOW;i<=CUT1_UP;i++){
+				    //loop over cut on dTheta(X-Y)
+  			            for(Int_t j=CUT2_LOW;j<=CUT2_UP;j++){
+					    if(dTh1X<=i && dThXY<=j){
+						    CutTopology4->Fill(i,j);
+					    }
+	  			    }//end cut2
+  			    }//end cut1
+			    
+		    }//end mode 15
+		    //station 123
+		    if (I("unp_trk_mode", itrack) == 14){
+			    dTh12=fabs(th1-th2);
+			    dTh13=fabs(th1-th3);
+			    dTh23=fabs(th2-th3);
+			    dTh1X=dTh12>dTh13?dTh12:dTh13;
+			    dThXY=dTh23;
+			    //no cut bin
+			    CutTopology3->Fill(CUT1_LOW-1,CUT2_LOW-1);
+			    //loop over cut on dTheta(1-X)
+                            for(Int_t i=CUT1_LOW;i<=CUT1_UP;i++){
+				    //loop over cut on dTheta(X-Y)
+  			            for(Int_t j=CUT2_LOW;j<=CUT2_UP;j++){
+					    if(dTh1X<=i && dThXY<=j){
+						    CutTopology3->Fill(i,j);
+					    }
+	  			    }//end cut2
+  			    }//end cut1
+			    
+		    }
+		    //station 124
+		    if (I("unp_trk_mode", itrack) == 13){
+			    dTh12=fabs(th1-th2);
+			    dTh14=fabs(th1-th4);
+	                    dTh24=fabs(th2-th4);
+			    dTh1X=dTh12>dTh14?dTh12:dTh14;
+			    dThXY=dTh24;
+			    //no cut bin
+			    CutTopology3->Fill(CUT1_LOW-1,CUT2_LOW-1);
+			    //loop over cut on dTheta(1-X)
+                            for(Int_t i=CUT1_LOW;i<=CUT1_UP;i++){
+				    //loop over cut on dTheta(X-Y)
+  			            for(Int_t j=CUT2_LOW;j<=CUT2_UP;j++){
+					    if(dTh1X<=i && dThXY<=j){
+						    CutTopology3->Fill(i,j);
+					    }
+	  			    }//end cut2
+  			    }//end cut1
+			    
+		    }
+		    //station 134
+		    if (I("unp_trk_mode", itrack) == 11){
+			    dTh13=fabs(th1-th3);
+			    dTh14=fabs(th1-th4);
+	                    dTh34=fabs(th3-th4);
+			    dTh1X=dTh13>dTh14?dTh13:dTh14;
+			    dThXY=dTh34;
+			    //no cut bin
+			    CutTopology3->Fill(CUT1_LOW-1,CUT2_LOW-1);
+			    //loop over cut on dTheta(1-X)
+                            for(Int_t i=CUT1_LOW;i<=CUT1_UP;i++){
+				    //loop over cut on dTheta(X-Y)
+  			            for(Int_t j=CUT2_LOW;j<=CUT2_UP;j++){
+					    if(dTh1X<=i && dThXY<=j){
+						    CutTopology3->Fill(i,j);
+					    }
+	  			    }//end cut2
+  			    }//end cut1
+		    }
+		    //station 234
+		    if (I("unp_trk_mode", itrack) == 7){
+			    dTh23=fabs(th2-th3);
+	                    dTh24=fabs(th2-th4);
+	                    dTh34=fabs(th3-th4);
+			    dThXY=dTh23>dTh24?dTh23:dTh24;
+			    dThXY=dThXY>dTh34?dThXY:dTh34;
+			    //no cut bin
+			    CutTopology3->Fill(CUT1_LOW-1,CUT2_LOW-1);
+			    //loop over cut on dTheta(1-X)
+                            for(Int_t i=CUT1_LOW;i<=CUT1_UP;i++){
+				    //loop over cut on dTheta(X-Y)
+  			            for(Int_t j=CUT2_LOW;j<=CUT2_UP;j++){
+					    if(dThXY<=j){
+						    CutTopology3->Fill(i,j);
+					    }
+	  			    }//end cut2
+  			    }//end cut1
+		    }
+		    
+		    
+	    }//end selection on track
+	     
+    }//end loop over unpacked tracks
+    
+  } // End loop events
+  std::cout << "\n******* Finished looping over the events *******" << std::endl;
+
+  delete in_chain;
+  std::cout << "\nDone with dThetaWindow(). Exiting.\n" << std::endl;
+	
+  //write to output file
+  TString outFile = "/afs/cern.ch/work/w/wshi/public/EMTFPileUp/dThetaWindow_pt_";
+  outFile = outFile + Form("%d", PT_CUT) +"_eta_"+Form("%.1f", ETA_LOW)+"_"+ Form("%.1f", ETA_UP)+ ".root";
+  TFile myPlot(outFile,"RECREATE");
+        
+  CutTopology4->GetXaxis()->SetTitle("dTheta(1-X)");
+  CutTopology4->GetYaxis()->SetTitle("dTheta(X-Y)");
+  CutTopology4->Write();
+        
+  CutTopology3->GetXaxis()->SetTitle("dTheta(1-X)");
+  CutTopology3->GetYaxis()->SetTitle("dTheta(X-Y)");
+  CutTopology3->Write();
+        
+  myPlot.Close();
+  
+} // End function: void NTuple_Analyzer()
